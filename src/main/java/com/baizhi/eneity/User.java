c963964f3e5bc16cee48b52078d9ae457515b1ad
yxj <1763538013@qq.com>
2018-06-08 16:08:04 +0800

    new class User

---
+++ src/main/java/com/baizhi/eneity/User.java
@@ -0,0 +1,47 @@
+package com.baizhi.eneity;
+
+import java.io.Serializable;
+
+/**
+ * Created by 于雪娇 on 2018/6/8.
+ */
+public class User implements Serializable{
+    private String name;
+    private Integer id;
+    private  String password;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public User() {
+
+    }
+
+    public User(String name, Integer id, String password) {
+
+        this.name = name;
+        this.id = id;
+        this.password = password;
+    }
+}
